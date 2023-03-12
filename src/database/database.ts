462760a3005d337b5b70330ce09d3ea06f53758e
lblake <treleven.lloyd@gmail.com>
2023-03-12 20:13:13 +0000

    updated database connection details

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,15 @@
-import { Sequelize } from "sequelize";
+import { Dialect, Sequelize } from "sequelize";
+export let sequelize = new Sequelize("sqlite::memory:");
+if (process.env.NODE_ENV !== "test") {
+	sequelize = new Sequelize(
+		process.env.DB_NAME ?? "MISSING_DB_NAME_CONFIG",
 
-// TODO: This should be external config
-export const sequelize = new Sequelize("sqlite::memory:");
+		process.env.DB_USERNAME ?? "MISSING_DB_USERNAME_CONFIG",
+
+		process.env.DB_PASSWORD ?? "MISSING_DB_PASSWORD_CONFIG",
+		{
+			host: process.env.DB_HOST ?? "MISSING_DB_HOST_CONFIG",
+			dialect: (process.env.DB_DIALECT as Dialect) ?? "postgres",
+		}
+	);
+}
